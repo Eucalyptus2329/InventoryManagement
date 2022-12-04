9e8767e8d7fbb0f25a6b49ec31699c1e2d7c2593
Eulix Chiu <eulix.chiu@gmail.com>
2022-12-04 10:02:03 -0800

    test

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -16,7 +16,7 @@ namespace InventoryManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Mainform());
+            Application.Run(new Loginform());
         }
     }
 }
