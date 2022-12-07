0de78f75d0a2acd6d10bd4e44da739ff45aecc39
Eulix Chiu <eulix.chiu@gmail.com>
2022-12-07 15:18:35 -0800

    Add UserEnumClass

---
+++ InventoryManagement/UserEnumClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public enum UserEnumClass
+    {
+        Add,
+        Update,
+        Delete,
+        Cancel
+    }
+}
