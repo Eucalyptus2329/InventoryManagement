72638e91df3ceb3a9c77d792be3ed17fe1907676
Eulix Chiu <eulix.chiu@gmail.com>
2022-12-07 15:17:58 -0800

    Add User Class

---
+++ InventoryManagement/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
